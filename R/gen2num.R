93a172e297d080b1085217aa74a166f12956aaa3
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2022-09-20 16:38:55 +0200

    Add gen2num function

---
+++ R/gen2num.R
@@ -0,0 +1,26 @@
+#' @title gen2num
+#'
+#' @description `gen2num` converts a generator into its corresponding column
+#' number.
+#'
+#' @param g A string or string vector.
+#' @return A numeric or numeric vector holding the numbers corresponding to the
+#' generators in `g`
+#' @examples
+#' gen2num('abc')
+#' col_numbers <- gen2num(g = c('acd','e','cdf'))
+#' @export
+gen2num <- function(g){
+  out <- c()
+  for (gen in g) {
+    # Check that g only contains lowercase letters
+    g_ascii <- utf8ToInt(gen)
+    if(any(g_ascii > 122 | g_ascii < 97)){
+      stop('Generators can only contain lowercase letters')
+    }
+    # Compute the corresponding number
+    n <- sum(2**(g_ascii-97))
+    out <- append(out, n)
+  }
+  return(out)
+}
