d23210e17c991f07aec713e73c6cd0fc9ef2f46e
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2022-09-23 10:13:47 +0200

    styling function code

--- R/gen2num.R
+++ R/gen2num.R
@@ -7,19 +7,19 @@
 #' @return A numeric or numeric vector holding the numbers corresponding to the
 #' generators in `g`
 #' @examples
-#' gen2num('abc')
-#' gen2num(g = c('acd','e','cdf'))
+#' gen2num("abc")
+#' gen2num(g = c("acd", "e", "cdf"))
 #' @export
-gen2num <- function(g){
+gen2num <- function(g) {
   out <- c()
   for (gen in g) {
     # Check that g only contains lowercase letters
     g_ascii <- utf8ToInt(gen)
-    if(any(g_ascii > 122 | g_ascii < 97)){
-      stop('Generators can only contain lowercase letters')
+    if (any(g_ascii > 122 | g_ascii < 97)) {
+      stop("Generators can only contain lowercase letters")
     }
     # Compute the corresponding number
-    n <- sum(2**(g_ascii-97))
+    n <- sum(2**(g_ascii - 97))
     out <- append(out, n)
   }
   return(out)
--- R/num2gen.R
+++ R/num2gen.R
@@ -7,22 +7,22 @@
 #' `n`
 #' @examples
 #' num2gen(7)
-#' num2gen(n = c(13,31,28))
+#' num2gen(n = c(13, 31, 28))
 #' @export
-num2gen <- function(n){
+num2gen <- function(n) {
   out <- c()
   for (x in n) {
     i <- 1
     index <- 1
     s <- ""
-    while(i <= x){
-      if(bitwAnd(i, x) != 0){
+    while (i <= x) {
+      if (bitwAnd(i, x) != 0) {
         s <- paste0(s, letters[index])
       }
-      i <- bitwShiftL(i,1)
+      i <- bitwShiftL(i, 1)
       index <- index + 1
     }
-    out <- append(out,s)
+    out <- append(out, s)
   }
   return(out)
 }
