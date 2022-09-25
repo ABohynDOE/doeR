7a3848ae42c8e572871c7d58a72d90b162305cb7
Alexandre Bohyn <alexandre.bohyn@kuleuven.be>
2022-09-25 17:41:54 +0200

    Add RcppArmadillo usage

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,4 @@
 # doeR 0.1.0
 
 * Added a `NEWS.md` file to track changes to the package.
+* Added basic design generating functions and tests
--- R/doeR-package.R
+++ R/doeR-package.R
@@ -1,4 +1,5 @@
 ## usethis namespace: start
 #' @useDynLib doeR, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
 ## usethis namespace: end
 NULL
